131982b472648ba9dee8937594fc66fdcf057587
juliastetskaya <juliastetskaya@gmail.com>
2019-06-22 15:04:34 +0300

    feat: initial setup webpack

---
+++ piskel-clone/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { enforce: 'pre', test: /\.js$/, loader: 'eslint-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Piskel Clone',
+    }),
+    new ExtractTextPlugin('app.css'),
+  ],
+};
